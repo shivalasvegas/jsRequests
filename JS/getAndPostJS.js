47d283cfcd5d75f585af4471d53253b85daf1265
shivalasvegas <shiva.king@academytrainee.com>
2020-07-23 22:00:07 +0100

    SK - getAndPostJS.js: created and edited for getAndPost.html

---
+++ JS/getAndPostJS.js
@@ -0,0 +1,99 @@
+const REQ = new XMLHttpRequest();
+
+function getTodos() {
+  REQ.onload = () => {
+    if (REQ.status === 200) {
+      console.log(REQ.response);
+      console.log(REQ.response.title);
+      for (let i = 0; i < 200; i++) {
+        document.querySelector("#resp").innerHTML = REQ.response[i].id;
+        console.log((document.querySelector("#resp").innerHTML = REQ.response[i].userId));
+        console.log((document.querySelector("#resp").innerHTML = REQ.response[i].id));
+        console.log((document.querySelector("#resp").innerHTML =REQ.response[i].title));
+        console.log((document.querySelector("#resp").innerHTML = REQ.response[i].completed)
+        );
+
+        let para = document.createElement("P"); // Create a <p> element
+        
+        para.className = "alert alert-primary col-md-8";
+        para.innerText = `The User id is : ${(document.querySelector(
+          "#resp"
+        ).innerHTML = REQ.response[i].id)} \n The User id is :  ${(document.querySelector(
+          "#resp"
+        ).innerHTML = REQ.response[i].userId)} \n  The title is : ${(document.querySelector(
+          "#resp"
+        ).innerHTML = REQ.response[i].title)}  \n  The completed status is : ${(document.querySelector(
+          "#resp"
+        ).innerHTML = REQ.response[i].completed)}`; // Insert text
+        let myDiv = document.getElementById("todos");
+        if (myDiv!=null) {myDiv.appendChild(para);}
+      }
+    } else {
+      console.log(`Handle Error!`);
+    }
+  };
+  REQ.open("GET", "http://jsonplaceholder.typicode.com/todos/");
+  REQ.setRequestHeader("Content-Type", "Application/json");
+  //This is to fix the cors ERROR
+  REQ.setRequestHeader("Access-Control-Allow-Origin", "*");
+  REQ.responseType = "json";
+  REQ.send();
+}
+
+
+// POSTING INFORMATION TO THE SERVER
+let data = '{"title": "Elegance","body": "Eloquance","userId": 1}';
+function postTodo() {
+  REQ.open("POST", "http://jsonplaceholder.typicode.com/posts");
+  REQ.setRequestHeader("Content-Type", "Application/json");
+  REQ.setRequestHeader("Access-Control-Allow-Origin", "*");
+  REQ.onload = () => {
+    if (REQ.status === 201) {
+      console.log(REQ.response);
+    } else {
+      console.log("handle error");
+    }
+  };
+  REQ.send(data); // What we want to send across
+}
+
+
+fetch('http://jsonplaceholder.typicode.com/todos')
+.then(
+  function(response) {
+    if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' +
+        response.status);
+      return;
+    }
+
+    // Examine the text in the response
+    response.json().then(function(data) {
+      console.log(data);
+      for (let i = 0; i < 200; i++) {
+        document.querySelector("#resp").innerHTML = data[i].id;
+        console.log(
+          (document.querySelector("#resp").innerHTML = data[i].userId)
+        );
+        console.log(
+          (document.querySelector("#resp").innerHTML = data[i].title)
+        );
+        console.log(
+          (document.querySelector("#resp").innerHTML =
+          data[i].completed)
+        );
+        console.log(
+          (document.querySelector("#resp").innerHTML =
+          data[i].completed)
+        );
+      }
+    });
+  }
+)
+.catch(function(err) {
+  console.log('Fetch Error :-S', err);
+});
+
+
+
+
